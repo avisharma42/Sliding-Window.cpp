327ee3cb1638fdc921330255561f2d951d60f279
avisharma42 <156396607+avisharma42@users.noreply.github.com>
2026-07-19 18:32:05 +0530

    Time: 13 ms (92.48%) | Memory: 139.1 MB (69.16%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n  = nums.size();
+        int i = 0; 
+        int j = 0;
+        vector<int> ans;
+        deque<int> q;
+        while(j<n){
+            while(!q.empty() && q.back()<nums[j]){
+                q.pop_back();
+             }  
+             q.push_back(nums[j]);
+             if(j-i+1 < k){
+                j++;
+             }       
+             else if (j-i+1 == k){
+                //calculate the answer
+                ans.push_back(q.front());
+                //sliding the window
+                if(nums[i]==q.front()){
+                    q.pop_front();
+                }
+                i++;
+                j++;
+             }  
+         
+         }
+         return ans;
+        }
+};
